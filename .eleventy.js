572481449fe0a799197a5624546f48aedbdb21e7
Sylvain Téchené <sylvain.techene@wanadoo.fr>
2021-11-20 17:47:17 +0100

    intégration netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,38 @@
+const yaml = require("js-yaml");
+const moment = require("moment");
+moment.locale("fr");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", (contents) => {
+    return yaml.load(contents);
+  });
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addHandlebarsShortcode("sortAndFilterByDate", (a) => {
+    const now = new Date();
+    return a
+      .sort((a, b) => {
+        const date1 = new Date(a.date);
+        const date2 = new Date(b.date);
+        return date1 - date2;
+      })
+      .filter((a) => a.date - now > 0)
+      .map((a) => ({
+        ...a,
+        formattedDate: {
+          weekDay: moment(a.date).format("dddd"),
+          day: moment(a.date).format("DD"),
+          month: moment(a.date).format("MMMM"),
+          year: moment(a.date).format("YYYY"),
+        },
+      }));
+  });
+  eleventyConfig.addHandlebarsShortcode("linkMaps", (payload) => {
+    const gjson = JSON.parse(payload);
+    return `http://maps.apple.com/?q=${gjson.coordinates[1]},${gjson.coordinates[0]}`;
+  });
 
   return {
     dir: {
